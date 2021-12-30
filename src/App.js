deac923a55c90d46b5b98fcefce36ab81a563dd0
evelynsba <evelynsba@gmail.com>
2021-12-30 09:41:17 -0300

    Criacao do projeto teste de final de modulo - Resilia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+// import NavBar from "./components/navBar/NavBar"
+import NavBar from "./components/Nav2"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from "./pages"
+import Agendamento from "./pages/Agendamento/agendamento"
+import Footer from "./components/Footer/Footer"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //  <NavBar/>
+
+     <Router>
+       <NavBar/>
+       <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/agendamento"  element={<Agendamento/>}/>
+       </Routes>
+       <Footer/>
+     </Router>
+  //   </div>
   );
 }
 
